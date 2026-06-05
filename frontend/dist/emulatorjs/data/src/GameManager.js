2be08de8de45272a6c7f33897fd8fd8332b3ddc0
osg27 <pagman35@gmail.com>
2026-06-05 21:40:03 +0100

    Fix Amiga AGA disk swapping

--- frontend/dist/emulatorjs/data/src/GameManager.js
+++ frontend/dist/emulatorjs/data/src/GameManager.js
@@ -156,6 +156,7 @@ class EJS_GameManager {
             "slowmotion_ratio = 3.0\n" +
             (this.EJS.rewindEnabled ? "rewind_enable = true\n" : "") +
             (this.EJS.rewindEnabled ? "rewind_granularity = 6\n" : "") +
+            "system_directory = \"/system\"\n" +
             "savefile_directory = \"/data/saves\"\n";
 
         if (this.EJS.retroarchOpts && Array.isArray(this.EJS.retroarchOpts)) {
