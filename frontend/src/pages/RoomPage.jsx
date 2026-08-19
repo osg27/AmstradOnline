4d50564e79dfece6ccc76686d457fbd317d8b687
osg27 <pagman35@gmail.com>
2026-08-20 00:59:52 +0100

    Revert "Select A500 ROMs for standard WHDLoad games"
    This reverts commit a1e8c93717ee82bcf5e6cf49f7c6d948085dcb66.

--- frontend/src/pages/RoomPage.jsx
+++ frontend/src/pages/RoomPage.jsx
@@ -1059,7 +1059,7 @@ export default function RoomPage() {
   }, [atari8Config]);
 
   const emulatorSrc = isPuaeAmiga
-    ? `/amiga-aga/launcher.html?model=${isAmigaAga ? 'A1200' : 'A500'}&v=2026-08-20-2`
+    ? `/amiga-aga/launcher.html?model=${isAmigaAga ? 'A1200' : 'A500'}&v=2026-08-20-1`
     : isAmigaLink
     ? '/amiga/launcher.html?v=2026-07-07-1'
     : isSegaConsole ? `/megadrive/launcher.html?system=${isMasterSystem ? 'mastersystem' : 'megadrive'}&v=2026-07-18-1` : isNes ? '/nes/launcher.html?v=2026-07-07-1' : isSnes ? '/snes/launcher.html?v=2026-08-09-1' : isPcEngine ? '/pcengine/launcher.html?v=2026-08-04-1' : isX68000 ? '/x68000/launcher.html?v=2026-08-05-6' : isPlayStation ? '/playstation/launcher.html?v=2026-07-07-1' : isBeetleSaturn ? '/webretro-saturn/index.html?core=yabause&nobundle&noautorefocus&v=2026-07-29-2' : isSaturn ? '/saturn/launcher.html?v=2026-07-27-3' : isC64 ? '/c64/launcher.html?v=2026-07-31-5' : isAtari8 ? atari8EmulatorSrc : isAtariSt ? '/atarist/launcher.html?v=2026-07-07-1' : isArcade ? '/arcade/launcher.html?v=2026-08-03-3' : isSpectrum ? '/spectrum/index.html?v=2026-08-03-1' : isCpcSystem ? '/emulator-cpcbox/index.html?v=2026-07-07-1' : '/emulator/index.html?v=2026-06-01-1';
@@ -6178,19 +6178,11 @@ export default function RoomPage() {
         );
       }
       const bytes = loadedFiles[0].bytes;
-      const isWhdLoadGame = Boolean(
-        loadedFiles[0]?.whdLoadArchive
-        || loadedFiles.some((loadedFile) => /\.(lha|slave)$/i.test(loadedFile.fileName)),
-      );
-      const whdLoadNeedsAga = isWhdLoadGame && loadedFiles.some((loadedFile) => (
-        /(?:^|[_\s(.-])(?:aga|cd32)(?:[_\s).-]|$)/i.test(
-          `${loadedFile.fileName || ''} ${loadedFile.archiveEntryName || ''}`,
-        )
-      ));
       const amigaModel = isPuaeAmiga
-        ? isWhdLoadGame
-          ? whdLoadNeedsAga ? 'A1200' : 'A500'
-          : isAmigaAga ? 'A1200' : 'A500'
+        ? (loadedFiles[0]?.whdLoadArchive
+          || loadedFiles.some((loadedFile) => /\.(lha|slave)$/i.test(loadedFile.fileName))
+          ? 'A1200'
+          : isAmigaAga ? 'A1200' : 'A500')
         : '';
       if (amigaModel && amigaModel !== amigaRequiredModel) {
         setAmigaRequiredModel(amigaModel);
